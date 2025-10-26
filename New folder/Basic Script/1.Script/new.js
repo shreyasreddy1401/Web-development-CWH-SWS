11dc301d9fc531ab53a10b7416c8663aacb67a26
shreyasreddy1401 <138093235+shreyasreddy1401@users.noreply.github.com>
2025-10-26 22:46:39 +0530

    Add files via upload

---
+++ New folder/Basic Script/1.Script/new.js
@@ -0,0 +1,6 @@
+console.log("Hello World")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+// install node js and run node new.js to see the output
---
+++ New folder/Basic Script/4.Script.js/script.js
@@ -0,0 +1,38 @@
+console.log("This is on loops")
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}   
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     company: "CodeWithHarry AI"
+// }
+ 
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
